b4e8da44bb26f15b6365a8caee93876d77df5f2f
Rani Abu Sultan <raniabusultan@gmail.com>
2018-12-23 23:12:03 +0300

    Groups

--- index.js
+++ index.js
@@ -4,20 +4,23 @@ const svg = canvas.append('svg')
   .attr('height', 600)
   .attr('width', 600);
 
-svg.append('rect')
+const group = svg.append('g')
+  .attr('transform', 'translate(50, 100)')
+
+group.append('rect')
   .attr('width', 200)
   .attr('height', 100)
   .attr('fill', 'blue')
   .attr('x', 20)
   .attr('y', 20)
 
-svg.append('circle')
+group.append('circle')
   .attr('r', 50)
   .attr('cx', 300)
   .attr('cy', 70)
   .attr('fill', 'pink')
 
-svg.append('line')
+group.append('line')
   .attr('x1', 370)
   .attr('x2', 400)
   .attr('y1', 20)
