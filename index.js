dae1d038237e5fa7956fd5cf1868446ed4ffbade
Rani Abu Sultan <raniabusultan@gmail.com>
2018-12-23 22:46:59 +0300

    Method Chaining & Attributes

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const canvas = d3.select(".canvas");
 
 const svg = canvas.append('svg')
+  .attr('height', 600)
+  .attr('width', 600);
 
-console.log(canvas)
+svg.append('rect')
+  .attr('width', 200)
+  .attr('height', 100)
+  .attr('fill', 'blue')
+  .attr('x', 20)
+  .attr('y', 20)
 
-// append shapes to svg container
-svg.append('rect');
-svg.append('circle');
-svg.append('line');
+svg.append('circle')
+  .attr('r', 50)
+  .attr('cx', 300)
+  .attr('cy', 70)
+  .attr('fill', 'pink')
+
+svg.append('line')
+  .attr('x1', 370)
+  .attr('x2', 400)
+  .attr('y1', 20)
+  .attr('y2', 120)
+  .attr('stroke', 'red')
