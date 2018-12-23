79533c945050af4b52a72296500e3ab47d314d1e
Rani Abu Sultan <raniabusultan@gmail.com>
2018-12-23 22:34:09 +0300

    Appending Elements

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// Select an item/s from the dom with pure JavaScript
-const a1 = document.querySelector('div');
-const a2 = document.querySelectorAll('div');
-console.log(a1,a2);
+const canvas = d3.select(".canvas");
 
-// Select an item/s from the dom with D3
-const b1 = d3.select('div')
-const b2 = d3.selectAll('div') 
-console.log(b1,b2);
+const svg = canvas.append('svg')
+
+console.log(canvas)
+
+// append shapes to svg container
+svg.append('rect');
+svg.append('circle');
+svg.append('line');
