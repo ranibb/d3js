9d9bb7f044e87e707bdb14d8040ebbcb768fcb57
Rani Abu Sultan <raniabusultan@gmail.com>
2019-01-31 10:42:00 +0300

    Shorten the arrow function

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const data = [
-  {width: 200, height: 100, fill: 'purple'}
+  { width: 200, height: 100, fill: 'purple' }
 ]
 
 const svg = d3.select('svg');
 
 const rect = svg.select('rect')
   .data(data)
-  .attr('width', (d, i, n) => {
-    console.log(this);
-    console.log(n[i]); // grab the current element
-    return d.width
-  })
-  .attr('height', function(d){
-    console.log(this) // grab the current element
-    return d.height
-  })
-  .attr('fill', function(d){return d.fill});
+  .attr('width', (d, i, n) => d.width)
+  .attr('height', d => d.height)
+  .attr('fill', d => d.fill);
 
   // console.log(rect)
