9dff6d69d07d9f04a88c491e76e2b95fa44bc011
Rani Abu Sultan <raniabusultan@gmail.com>
2019-01-31 10:30:45 +0300

    How the 'this' keyword is bound when using arrow function Vs regular function

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const svg = d3.select('svg');
 
 const rect = svg.select('rect')
   .data(data)
-  .attr('width', function(d, i, n){
-    console.log(i);
-    console.log(n);
+  .attr('width', (d, i, n) => {
+    console.log(this);
+    console.log(n[i]); // grab the current element
     return d.width
   })
-  .attr('height', function(d){return d.height})
+  .attr('height', function(d){
+    console.log(this) // grab the current element
+    return d.height
+  })
   .attr('fill', function(d){return d.fill});
 
-  console.log(rect)
+  // console.log(rect)
