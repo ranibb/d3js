d0bda22af7625db9aa5f858ea5172654d59f76af
Rani Abu Sultan <raniabusultan@gmail.com>
2019-01-31 10:21:51 +0300

    console log the rect index i and the current selection n

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const svg = d3.select('svg');
 
 const rect = svg.select('rect')
   .data(data)
-  .attr('width', function(d){return d.width})
+  .attr('width', function(d, i, n){
+    console.log(i);
+    console.log(n);
+    return d.width
+  })
   .attr('height', function(d){return d.height})
   .attr('fill', function(d){return d.fill});
 
