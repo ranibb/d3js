d450732b69a4571dce64cfc2b679fec6123fd886
Rani Abu Sultan <raniabusultan@gmail.com>
2019-02-03 11:14:51 +0300

    External Data (JSON)

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const data = [
-  { width: 200, height: 100, fill: 'purple' },
-  { width: 100, height: 60, fill: 'pink' },
-  { width: 50, height: 30, fill: 'red' }
-]
-
+// select svg container
 const svg = d3.select('svg');
 
-// Join data to rects
-const rect = svg.selectAll('rect')
-  .data(data)
+d3.json('planets.json').then(data => {
 
-// add attrs to rects already in the DOM
-rect.attr('width', (d, i, n) => d.width)
-  .attr('height', d => d.height)
-  .attr('fill', d => d.fill);
+  const circs = svg.selectAll('circle')
+    .data(data);
 
-// append the enter selection to DOM
-rect.enter()
-  .append('rect')
-  .attr('width', (d, i, n) => d.width)
-  .attr('height', d => d.height)
-  .attr('fill', d => d.fill);
+  // add attrs to circs already in DOM
+  circs.attr('cy', 200)
+    .attr('cx', d => d.distance)
+    .attr('r', d => d.radius)
+    .attr('fill', d => d.fill)
 
-  console.log(rect)
+  // append the enter selection to the DOM
+  circs.enter()
+    .append('circle')
+      .attr('cy', 200)
+      .attr('cx', d => d.distance)
+      .attr('r', d => d.radius)
+      .attr('fill', d => d.fill)
+});
