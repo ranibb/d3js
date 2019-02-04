81d4bbde1b8edefd41c337acd84f2abe6ada0318
Rani Abu Sultan <raniabusultan@gmail.com>
2019-02-04 11:12:19 +0300

    Creating a Bar Chart ( Groups & Margins)

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 // select svg container
-const svg = d3.select('svg');
+const svg = d3.select('.canvas')
+  .append('svg')
+    .attr('width', 600)
+    .attr('height', 600)
+
+// create margins and dimensions
+const margin = {top: 20, right: 20, bottom: 100, left: 100}
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
 d3.json('menu.json').then(data => {
 
@@ -31,7 +44,7 @@ d3.json('menu.json').then(data => {
   // console.log(y(900));
 
   // join the data to rects
-  const rects = svg.selectAll('rect')
+  const rects = graph.selectAll('rect')
     .data(data);
 
   // add attrs to rects already in DOM
